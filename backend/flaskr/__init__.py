cfbc7151775470459850f6844d2fbb72a13f77f2
Jayaraj J <jayarajevur@gmail.com>
2020-10-06 07:15:41 +0530

    added search endpoint

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -158,10 +158,15 @@ def create_app(test_config=None):
   It should return any questions for whom the search term 
   is a substring of the question. 
 
+
   TEST: Search by any phrase. The questions list will update to include 
   only question that include that string within their question. 
   Try using the word "title" to start. 
   '''
+  @app.route('/questions', methods=['POST'])
+  def search_question():
+        
+
 
   '''
   @TODO: 
